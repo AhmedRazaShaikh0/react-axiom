dbd17669b788dd22dc9f7bbc540fa598b0caff97
AhmedRazaShaikh <110739149+xtylershona@users.noreply.github.com>
2023-03-04 01:16:42 +0500

    anim

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero(hero:sec) {
     return (
         <div className=' relative flex items-center justify-center overflow-hidden h-screen'>
             <div className='z-30 relative text-center'>
-                <h1 className=' text-white sm:text-7xl text-4xl font-bold fadeUp overflow-hidden'>{hero.title}</h1>
+                <h1 className=' text-white sm:text-7xl text-4xl font-bold fadeUp'>{hero.title}</h1>
                 <button className=' border-[2px] border-white sm:px-[25px] px-[15px] sm:py-[10px] py-[5px] text-white hover:text-black hover:bg-white mt-[25px] font-bold text-lg'><IoPlay className='inline -mt-[5px] text-xl mr-[5px]' />Watch</button>
             </div>
             <video className='absolute w-auto min-w-full min-h-full max-w-none' src={video} autoPlay loop muted></video>
--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import '../index.css'
 
 interface sec {
     subTitle: string,
@@ -6,18 +6,38 @@ interface sec {
     desc: string
 }
 
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++ ){
+       var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+reveal()
+
 export default function Shop(shop:sec) {
     return (
 
-        <div className=' w-[80%] flex justify-between m-auto sm:flex-row flex-col-reverse h-[15cm]'>
+        <div className=' w-[80%] flex justify-between mb-[350px] sm:mb-[0px] m-auto sm:flex-row flex-col-reverse h-[15cm]'>
             <div className='flex relative lg:w-[15cm]'>
                 <img src="/img1.webp" alt="" className='absolute top-[30px] sm:top-[50px] left-[80px] sm:left-[140px] z-10 shadow-2xl w-[140px] sm:w-[280px]' />
-                <img src="/img3.jpg" alt="" className='absolute top-[0px] sm:top-[10px] left-[10px] sm:left-[20px] w-[90px] sm:w-[180px]' />
-                <img src="/img2.jpg" alt="" className='absolute top-[140px] sm:top-[240px] left-[30px] sm:left-[40px] w-[100px] sm:w-[200px]' />
-                <img src="/img5.jpg" alt="" className='absolute top-[40px] sm:top-[90px] left-[210px] sm:left-[400px] w-[50px] sm:w-[100px]' />
-                <img src="/img4.jpg" alt="" className='absolute top-[140px] sm:top-[260px] left-[200px] sm:left-[400px] w-[80px] sm:w-[160px]' />
+                <img src="/img3.jpg" alt="" className='absolute top-[0px] sm:top-[10px] left-[10px] sm:left-[20px] w-[90px] sm:w-[180px] reveal' />
+                <img src="/img2.jpg" alt="" className='absolute top-[140px] sm:top-[240px] left-[30px] sm:left-[40px] w-[100px] sm:w-[200px] reveal' />
+                <img src="/img5.jpg" alt="" className='absolute top-[40px] sm:top-[90px] left-[210px] sm:left-[400px] w-[50px] sm:w-[100px] reveal' />
+                <img src="/img4.jpg" alt="" className='absolute top-[140px] sm:top-[260px] left-[200px] sm:left-[400px] w-[80px] sm:w-[160px] reveal' />
             </div>
-            <div className=' sm:w-1/3 my-auto'>
+            <div className=' sm:w-1/3 my-auto '>
                 <h2 className=' text-[17px] tracking-widest text-gray-700'>{shop.subTitle}</h2>
                 <h1 className=' text-[30px] font-bold leading-[35px] mt-[5px]'>{shop.title}</h1>
                 <p className=' text-[16px] text-gray-700 mt-[20px]'>{shop.desc}</p>
