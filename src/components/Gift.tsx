f320440ac2a8a730a229d6bbd1be53d07031d5da
Ahmed Raza <ahmedraza6100@gmail.com>
2023-03-16 02:08:58 +0500

    doem

--- src/components/Gift.tsx
+++ src/components/Gift.tsx
@@ -15,7 +15,7 @@ const giftData: giftType = data;
 export default function Gift() {
     return (
         <div className='bg-white flex md:flex-row flex-col-reverse m-auto max-w-6xl my-32 justify-between px-[3%]'>
-            <div className=' md:w-[50%] w-full md:pr-20 m-auto mt-10 md:mt-0'>
+            <div className=' md:w-[50%] w-full md:pr-20 m-auto mt-10 md:mt-0 lg:mt-12'>
                 <h2 className=' text-[17px] tracking-widest text-gray-700'>{giftData.subTitle}</h2>
                 <h1 className=' text-[30px] font-bold leading-[35px] mt-[5px]'>{giftData.title}</h1>
                 <p className=' text-[16px] text-gray-700 mt-[20px]'>{giftData.desc}</p>
